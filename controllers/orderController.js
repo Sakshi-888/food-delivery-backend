9f6132014d59263ed0aedf5ef2a08df0aa6f6dc3
Sakshi Sharma <sharmasakshi8546@gmail.com>
2024-07-12 16:19:56 +0530

    All set!

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend
 const placeOrder = async (req,res) =>{
 
-    const frontend_URL = 'https://food-delivery-tomato.vercel.app';
+    const frontend_url = 'https://food-delivery-tomato.vercel.app/';
 
 
     try {
@@ -44,8 +44,8 @@ const placeOrder = async (req,res) =>{
        })
 
        const session = await stripe.checkout.sessions.create({
-        success_url:`${frontend_URL}/verify?success=true&orderId=${newOrder._id}`,
-        cancel_url:`${frontend_URL}/verify?success=false&orderId=${newOrder._id}`,
+        success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+        cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
         line_items:line_items,
         mode:'payment',
 
@@ -77,4 +77,37 @@ const verifyOrder = async (req,res) =>{
      }
 }
 
-export {placeOrder,verifyOrder}
+// user orders for frontend
+const userOrders = async (req,res) => {
+try {
+    const orders = await orderModel.find({userId:req.body.userId});
+    res.json({success:true,data:orders})
+} catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+}
+}
+
+// Listing orders for admin panel
+const listOrders = async (req,res) =>{
+   try {
+    const orders = await orderModel.find({});
+    res.json({success:true,data:orders})
+   } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+   }
+}
+
+// api for updating order status
+const updateStatus = async (req,res) => {
+try {
+    await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+    res.json({success:true,message:"Status Updated"})
+} catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+}
+}
+
+export {placeOrder,verifyOrder,userOrders,listOrders,updateStatus}
